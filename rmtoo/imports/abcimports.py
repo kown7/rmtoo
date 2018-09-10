51af9ea0fb7c3a2e6bfe713db30b407f3c95152e
Kristoffer Nordstroem <kristoffer@nordstroem.ch>
2018-09-11 00:08:17 +0200

    py27 compatibility

--- rmtoo/imports/abcimports.py
+++ rmtoo/imports/abcimports.py
@@ -2,11 +2,16 @@
 
 '''
 import abc
+import sys
+
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:
+    ABC = abc.ABCMeta('ABC', (object,), {}) # compatible with Python 2 *and* 3
 
 
-class AbcImports(abc.ABC):
+class AbcImports(ABC):
     '''Define an ABC for all imports classes'''
-
 
     @abc.abstractmethod
     def __init__(self, self_cfg, import_dest):
